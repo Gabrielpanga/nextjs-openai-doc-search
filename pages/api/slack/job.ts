f343dbaed77435fce83efbfbbe2ac61736360d41
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2023-10-09 17:57:44 +0200

    fix: process on background second function

--- pages/api/slack/job.ts
+++ pages/api/slack/job.ts
@@ -18,14 +18,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     console.log(`Received question: ${question}, starting to process`)
-    const response = await getResponse(question)
-    const result = await response.text()
-    console.log('Result: ', result)
-    await sendMessageResponse(response_url || '', {
-      text: `Here is what i found: \n\n ${result}`,
+    getResponse(question).then(async (response) => {
+      const result = await response.text()
+      console.log('Result: ', result)
+      await sendMessageResponse(response_url || '', {
+        text: `Here is what i found: \n\n ${result}`,
+      })
     })
 
-    return res.status(200).json({ result })
+    return res.status(200).json({ message: 'Job sent to queue' })
   } catch (error) {
     console.error(error)
     return res.status(500).json({ error: 'Internal server error' })
